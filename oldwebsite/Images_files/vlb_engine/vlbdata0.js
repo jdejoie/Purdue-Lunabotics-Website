918378cd35a10d00563114737da8516bd4930c77
Julianne Dejoie <jdejoie@purdue.edu>
2018-06-16 20:29:51 -0400

    added items to code

---
+++ oldwebsite/Images_files/vlb_engine/vlbdata0.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:false,borderSize:57,classNames:'vlightbox0',closeLocation:'top',descSliding:false,enableRightClick:false,enableSlideshow:true,overlayOpacity:0,prefix:'vlb0',resizeSpeed:7,slideTime:4,startZoom:true}) });
---
+++ oldwebsite/Images_files/vlb_engine/vlbdata7.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:false,borderSize:57,classNames:'vlightbox7',closeLocation:'top',descSliding:false,enableRightClick:false,enableSlideshow:true,prefix:'vlb7',resizeSpeed:7,slideTime:4,startZoom:true}) });
